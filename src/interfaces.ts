9768ca124eaab5c222fc654e61ea474ade567cb1
lukbar48 <lukaszb48@gmail.com>
2021-12-12 21:32:27 +0100

    Added mobile navbar

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,8 +1,8 @@
 import { ReactNode } from 'react';
 
 export interface IButton { 
-  backgroundColor?: string;
   children: ReactNode;
+  backgroundColor?: string;
   width?: string;
   height?: string;
   live?: boolean;
